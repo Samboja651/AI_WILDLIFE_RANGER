37975366afe6d49932d4d0c5338198c725459bb2
Samboja651 <waribekihiko651@gmail.com>
2025-03-05 07:33:35 +0300

    enhanced schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,21 @@
-CREATE DATABASE HWC_DB;
-
-USE HWC_DB;
-
-CREATE TABLE kibocheRTData(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- dbms = mysql
+CREATE DATABASE IF NOT EXISTS WDF_conservation;
+USE WDF_conservation;
+-- realtime data for lion named kiboche
+CREATE TABLE IF NOT EXISTS kibocheRTData(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     timestamp DATETIME NOT NULL,
     location_long FLOAT NOT NULL,
     location_lat FLOAT NOT NULL,
-    individual_local_identifier VARCHAR(20) NOT NULL, -- i don't think this column data is important.
     time_interval_hours FLOAT NOT NULL
 );
-
-CREATE TABLE reportData(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS reportData(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     predictions_made INT NOT NULL,
     correct_predictions INT NOT NULL,
     failed_predictions INT NOT NULL,
     success_rate FLOAT NOT NULL
 );
+CREATE USER 'wdf_conservatist'@'localhost' IDENTIFIED BY '@WildlifeTech2025'; -- create another user instead of root operations.
+GRANT ALL PRIVILEGES ON WDF_conservation.* TO 'wdf_conservatist'@'localhost';
+-- end
